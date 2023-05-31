1ee98bdbeca0543c6c7a5f5c2bb1aee8fee7481c
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-06-01 08:56:41 +0900

    초기 커밋.

---
+++ Cat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InheritancePrac3_2
+{
+    internal class Cat
+    {
+        internal void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Sleep()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Meow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InheritancePrac3_2
+{
+    public class Dog
+    {
+        public int Age { get, set; }
+         public string Color { get; set; }
+
+        public Dog() { this.Age = 0};
+
+        public void Bark()
+        {
+            Console.WriteLine("왈왈 짖습니다.");
+        }
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sleep()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
